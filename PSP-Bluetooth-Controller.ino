4fd25f59b62e50789683c4f17040ae5379cce19d
Stephen Cooper <ste.cooper@live.co.uk>
2022-06-07 07:58:29 +0100

    Analog stick (#1)
    Added hardware V1
    
    Co-authored-by: asd <asd.asd@asd.co>

--- PSP-Bluetooth-Controller.ino
+++ PSP-Bluetooth-Controller.ino
@@ -1,4 +1,5 @@
 #include <Bluepad32.h>
+#include <SPI.h>
 
 // Currently only use the first connected gamepad. Bluepad32 supports upto 4
 // Would be good to allow multiple, maybe could use different controllers for emulators etc
@@ -50,51 +51,121 @@ typedef struct {
 
 pinMap mappedPins[] = {
   // Bottom buttons
-  { 15, MISC_BUTTON_SYSTEM, false, TYPE_SYSTEM }, // home - D15 - Pin 20
-  { 14, MISC_BUTTON_HOME, false, TYPE_SYSTEM }, // start - D14 - Pin 19
-  { 13, MISC_BUTTON_BACK, false, TYPE_SYSTEM }, // select - D13 - Pin 16
-  { 12, BUTTON_THUMB_R, false, TYPE_BUTTONS },  // screen - D12 - Pin 15
+  { 14, MISC_BUTTON_SYSTEM, false, TYPE_SYSTEM }, // home - D15 - Pin 20
+  { 17, MISC_BUTTON_HOME, false, TYPE_SYSTEM }, // start - D14 - Pin 19
+  { 16, MISC_BUTTON_BACK, false, TYPE_SYSTEM }, // select - D13 - Pin 16
+  { 15, BUTTON_THUMB_R, false, TYPE_BUTTONS },  // screen - D12 - Pin 15
 
   // Left hand buttons
-  { 11, BUTTON_SHOULDER_L, false, TYPE_BUTTONS  }, // Left Shoulder Button - D11 - pin 14
-  { 10, DPAD_UP, false, TYPE_DPAD  }, // up - D10 - pin 13
-  { 9, DPAD_DOWN, false, TYPE_DPAD  }, // down - D9 - pin 12
-  { 8, DPAD_RIGHT, false, TYPE_DPAD  }, // right - D8 - Pin 11
-  { 7, DPAD_LEFT, false, TYPE_DPAD  }, // left - D7 - Pin 10
+  { 19, BUTTON_SHOULDER_L, false, TYPE_BUTTONS  }, // Left Shoulder Button - D11 - pin 14
+  { 9, DPAD_UP, false, TYPE_DPAD  }, // up - D10 - pin 13
+  { 7, DPAD_DOWN, false, TYPE_DPAD  }, // down - D9 - pin 12
+  { 18, DPAD_RIGHT, false, TYPE_DPAD  }, // right - D8 - Pin 11
+  { 8, DPAD_LEFT, false, TYPE_DPAD  }, // left - D7 - Pin 10
   
   // Right hand buttons
-  { 6, BUTTON_SHOULDER_R, false, TYPE_BUTTONS }, // Right shoulder Button - D6 - Pin 9
-  { 5, BUTTON_A, false, TYPE_BUTTONS  }, // Cross - D5 - pin 8
-  { 4, BUTTON_B, false, TYPE_BUTTONS  }, // Circle - D4 - Pin 7
-  { 3, BUTTON_X, false, TYPE_BUTTONS  }, // Square - D3 - Pin 6
-  { 2, BUTTON_Y, false, TYPE_BUTTONS  } // Triangle - D2 - Pin 5
+  { 5, BUTTON_SHOULDER_R, false, TYPE_BUTTONS }, // Right shoulder Button - D6 - Pin 9
+  { 2, BUTTON_A, false, TYPE_BUTTONS  }, // Cross - D5 - pin 8
+  { 3, BUTTON_B, false, TYPE_BUTTONS  }, // Circle - D4 - Pin 7
+  { 6, BUTTON_X, false, TYPE_BUTTONS  }, // Square - D3 - Pin 6
+  { 4, BUTTON_Y, false, TYPE_BUTTONS  } // Triangle - D2 - Pin 5
 };
 const byte mappedPinsSize = sizeof(mappedPins) / sizeof(pinMap);
 
-void setup() {
+const int slaveSelectPin = 10;
+const int wiper0writeAddr = B00000000; // 0b00000000;
+const int wiper1writeAddr = B00010000;// 0b00010000;
 
-  Serial.begin(9600);
-  
-  while (!Serial) {
-    ;
-  }
-  
-  String fv = BP32.firmwareVersion();
-  Serial.print("Firmware version installed: ");
-  Serial.println(fv);
+int currentX = 0;
+int currentY = 0;
 
-  BP32.setup(&onConnectedGamepad, &onDisconnectedGamepad);
+byte PSPPoweredOn = 0;
 
-  // This needs to be behind a reset button later
-  BP32.forgetBluetoothKeys();
+void togglePower(){
+  pressPin(21);
+  delay(500);
+  pinMode(21, INPUT);
 }
 
+void toggleScreen(){
+  pressPin(15);
+  delay(5000);
+  pinMode(15, INPUT);
+}
 
+void powerOn() {
+  if (PSPPoweredOn == 0){
+    togglePower();
+    PSPPoweredOn = 1;
+    delay(1000);
+    toggleScreen();
+  }
+}
+
+void powerOff() {
+  if (PSPPoweredOn == 1){
+    togglePower();
+    PSPPoweredOn = 0;
+  }
+}
+
+/*
+ * --------------------------------
+ * Analog stick related methods
+ * --------------------------------
+ */
+
+void setPot(int x, int y) {
+  int xToSet = constrain(x, 0, 255);
+  int yToSet = constrain(y, 0, 255);
+  
+  if (xToSet != currentX) {
+    digitalPotWrite(wiper1writeAddr, xToSet);
+    currentX = xToSet;
+  }
+
+  if (yToSet != currentY) {
+    digitalPotWrite(wiper0writeAddr, yToSet);
+    currentY = yToSet;
+  }
+}
+
+// Should really use a lib for this
+void digitalPotWrite(int address, int value) {
+  // take the SS pin low to select the chip:
+  digitalWrite(slaveSelectPin,LOW);
+  //  send in the address and value via SPI:
+  SPI.transfer(address);
+  SPI.transfer(value);
+  // take the SS pin high to de-select the chip:
+  digitalWrite(slaveSelectPin,HIGH); 
+}
+
+void updateLeftStick(GamepadPtr gamepad) {
+    int axisX = gamepad->axisX() + 511;
+    int axisY = gamepad->axisY() + 511;
+
+    // For some reason PSP reports full range from a value of 50 - 200.
+    int axisXMapped = map(axisX, 0, 1023, 50, 200);
+    int axisYMapped = map(axisY, 0, 1023, 50, 200);
+
+    setPot(axisXMapped, axisYMapped);
+}
+
+//---------------------------------------
+
+/*
+ * --------------------------------------
+ * Bluepad32 callbacks
+ * --------------------------------------
+ */
 void onConnectedGamepad(GamepadPtr gp) {
   if (myGamepad == nullptr) {
     myGamepad = gp;
     
     Serial.println("CALLBACK: Gamepad is connected");
+
+    powerOn();
   }
 }
 
@@ -103,26 +174,31 @@ void onDisconnectedGamepad(GamepadPtr gp) {
     myGamepad = nullptr;
     
     Serial.println("CALLBACK: Gamepad is disconnected");
+
+    powerOff();
   }
 }
 
-void releaseUnusedPins(){
-  for(byte i = 0; i < mappedPinsSize; i++){
-    if (mappedPins[i].pressed == false){
-      pinMode(mappedPins[i].pin, INPUT);
-    }
-  }
-}
+//------------------------------------------
 
-void resetPinPressed() {
+/*
+ * -----------------------------------------
+ * Button handling methods
+ * -----------------------------------------
+ */
+void releaseAllPins() {
   for(byte i = 0; i < mappedPinsSize; i++){
     mappedPins[i].pressed = false;
+    releasePin(mappedPins[i].pin);
   }
 }
 
-void pressPin(byte pin) {  
-    Serial.println("PRessing pin:");
-    Serial.println(pin);
+void releasePin(byte pin) {
+  pinMode(pin, INPUT);
+}
+
+void pressPin(byte pin) { 
+    Serial.println(pin);     
     
     pinMode(pin, OUTPUT);
     digitalWrite(pin, LOW);
@@ -135,42 +211,88 @@ void updateButtons(GamepadPtr gamepad) {
     switch (mapping->type) {
       case TYPE_DPAD:
         if (gamepad->dpad() & mapping->controllerMask) {
-          mapping->pressed = true;
-          pressPin(mapping->pin);
+          if (!mapping->pressed) {
+            mapping->pressed = true;
+            pressPin(mapping->pin);
+          }
         } else {
-          mapping->pressed = false;
+          if (mapping->pressed) {
+            mapping->pressed = false;
+            releasePin(mapping->pin);
+          }
         }
       break;
       case TYPE_BUTTONS:
         if (gamepad->buttons() & mapping->controllerMask) {
-          mapping->pressed = true;
-          pressPin(mapping->pin);
+          if (!mapping->pressed) {
+            mapping->pressed = true;
+            pressPin(mapping->pin);
+          }
         } else {          
-          mapping->pressed = false;
+          if (mapping->pressed) {
+            mapping->pressed = false;
+            releasePin(mapping->pin);
+          }
         }
       break;
       case TYPE_SYSTEM:
         if (gamepad->miscButtons() & mapping->controllerMask) {
-          mapping->pressed = true;
-          pressPin(mapping->pin);
+          if (!mapping->pressed) {
+            mapping->pressed = true;
+            pressPin(mapping->pin);
+          }
         } else {          
-          mapping->pressed = false;
+          if (mapping->pressed) {
+            mapping->pressed = false;
+            releasePin(mapping->pin);
+          }
         }
       break; 
     }
   }
 }
 
-void loop() {
+// -------------------------------------------------
+
+/*
+ * ----------------------------------------
+ * Arduino lifecycle methods
+ * ----------------------------------------
+ */
+
+void setup() {
+  Serial.begin(9600);
+    
+  String fv = BP32.firmwareVersion();
+  BP32.setup(&onConnectedGamepad, &onDisconnectedGamepad);
+  
+  Serial.println("Setup");
+  Serial.println(fv);
+
+  // This needs to be behind a reset button later
+  BP32.forgetBluetoothKeys();
+
+  pinMode(slaveSelectPin, OUTPUT);
+  SPI.begin(); 
+  
+  setPot(127, 127);
+  
+  Serial.println("Setup Complete");
+}
+
+void loop() {  
   BP32.update();
   
   if (myGamepad && myGamepad->isConnected()) {  
     updateButtons(myGamepad);
+    updateLeftStick(myGamepad);
+
+    if (myGamepad->thumbL())
+      togglePower();
+          
   } else {
-    resetPinPressed();
-  }
-  
-  releaseUnusedPins();
-  
-  delay(3);
+    releaseAllPins();
+  } 
 }
+
+//--------------------------------------
