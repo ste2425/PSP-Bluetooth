7bae3df448e19efc525299c37f8be468d931cfab
Stephen Cooper <ste.cooper@live.co.uk>
2024-06-17 13:31:12 +0100

    Sudo all the thngs

--- software/scripts/install.sh
+++ software/scripts/install.sh
@@ -5,14 +5,14 @@ SCRIPTS_DIR="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
 echo "Updating index..."
 
-arduino-cli core update-index --log --config-file ../arduino-cli.yaml 
+sudo arduino-cli core update-index --log --config-file ../arduino-cli.yaml 
 
 echo "Installing core..."
 
-arduino-cli core install esp32-bluepad32:esp32 --log --config-file ../arduino-cli.yaml 
+sudo arduino-cli core install esp32-bluepad32:esp32 --log --config-file ../arduino-cli.yaml 
 
 echo "Installing Libraries..."
 
-arduino-cli lib install ezButton --log --config-file ../arduino-cli.yaml 
+sudo arduino-cli lib install ezButton --log --config-file ../arduino-cli.yaml 
 
 echo "Done"
