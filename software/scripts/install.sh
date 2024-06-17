1c7bfb2c1f9350430445360e148dc0b8a702feac
Stephen Cooper <ste.cooper@live.co.uk>
2024-06-17 13:27:07 +0100

    Initial Arduino install script

--- software/scripts/install.sh
+++ software/scripts/install.sh
@@ -3,4 +3,16 @@
 # https://stackoverflow.com/a/4774063
 SCRIPTS_DIR="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
-echo "hello"
+echo "Updating index..."
+
+arduino-cli core update-index --log --config-file ../arduino-cli.yaml 
+
+echo "Installing core..."
+
+arduino-cli core install esp32-bluepad32:esp32 --log --config-file ../arduino-cli.yaml 
+
+echo "Installing Libraries..."
+
+arduino-cli lib install ezButton --log --config-file ../arduino-cli.yaml 
+
+echo "Done"
