8a54a5717f14c101dd746e11f0b52491e1641bd0
Stephen Cooper <ste.cooper@live.co.uk>
2024-06-17 13:54:35 +0100

    t

--- software/scripts/compile.sh
+++ software/scripts/compile.sh
@@ -6,6 +6,12 @@ echo "Compiling"
 SCRIPTS_DIR="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 BIN_DIR="$SCRIPTS_DIR/../bin"
 PROJECT_BIR="$SCRIPTS_DIR/../"
+YAML_NAME="../arduino-cli.yaml"
+
+if [ x"${GITHUB_ACTION}" == "true" ]; then
+    echo "Using runner yaml"
+    YAML_NAME="../arduino-cli-runner.yaml"
+fi
 
 echo "   Bin DIR: $BIN_DIR"
 echo "   Project DIR: $PROJECT_BIR"
@@ -14,6 +20,6 @@ echo "Deleting bin..."
 rm -r $BIN_DIR
 
 echo "Compiling..."
-arduino-cli compile --log --fqbn esp32-bluepad32:esp32:esp32 --output-dir $BIN_DIR $PROJECT_BIR --config-file ../arduino-cli.yaml 
+arduino-cli compile --log --fqbn esp32-bluepad32:esp32:esp32 --output-dir $BIN_DIR $PROJECT_BIR --config-file $YAML_NAME 
 
 echo "Done"
--- software/scripts/install.sh
+++ software/scripts/install.sh
@@ -2,17 +2,23 @@
 
 # https://stackoverflow.com/a/4774063
 SCRIPTS_DIR="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+YAML_NAME="../arduino-cli.yaml"
+
+if [ x"${GITHUB_ACTION}" == "true" ]; then
+    echo "Using runner yaml"
+    YAML_NAME="../arduino-cli-runner.yaml"
+fi
 
 echo "Updating index..."
 
-arduino-cli core update-index --log --additional-urls "https://raw.githubusercontent.com/ricardoquesada/esp32-arduino-lib-builder/master/bluepad32_files/package_esp32_bluepad32_index.json,https://raw.githubusercontent.com/espressif/arduino-esp32/gh-pages/package_esp32_index.json"
+arduino-cli core update-index --log --config-file $YAML_NAME
 
 echo "Installing core..."
 
-arduino-cli core install esp32-bluepad32:esp32 --log
+arduino-cli core install esp32-bluepad32:esp32 --log --config-file $YAML_NAME
 
 echo "Installing Libraries..."
 
-arduino-cli lib install ezButton --log
+arduino-cli lib install ezButton --log --config-file $YAML_NAME
 
 echo "Done"
