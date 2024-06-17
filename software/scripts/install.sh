d198090ec792ba66bd791c4fe1e9c17a4bc65325
Stephen Cooper <ste.cooper@live.co.uk>
2024-06-17 13:38:39 +0100

    t

--- software/scripts/install.sh
+++ software/scripts/install.sh
@@ -5,11 +5,11 @@ SCRIPTS_DIR="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
 echo "Updating index..."
 
-arduino-cli core update-index --log 
+arduino-cli core update-index --log --additional-urls "https://raw.githubusercontent.com/ricardoquesada/esp32-arduino-lib-builder/master/bluepad32_files/package_esp32_bluepad32_index.json,https://raw.githubusercontent.com/espressif/arduino-esp32/gh-pages/package_esp32_index.json"
 
 echo "Installing core..."
 
-arduino-cli core install arduino:samd --log 
+arduino-cli core install esp32-bluepad32:esp32 --log
 
 echo "Installing Libraries..."
 
