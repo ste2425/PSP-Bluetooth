3648705b500e3d6d0e74ec96b5103532eb4be54c
Stephen Cooper <ste.cooper@live.co.uk>
2024-11-08 20:37:29 +0000

    Initially fully working PSP Bluetooth
    **NOTE**
    
    * This is hard coded for consolizers
    * This does not support OTA
    * This is hard coded to forget paired controllers on power cycle
    * This is very much early alpha release - expect issues

---
+++ software/libraries/readme.md
@@ -0,0 +1,9 @@
+Probably a better way to do this.
+
+This stores libraries in zip format which cannot be installed via the package manager.
+
+The `install.sh` file installs these libraries.
+
+| library | url | version
+| -- | -- | -- |
+| MCP_POT |https://github.com/RobTillaart/MCP_POT | 0.2.2 |
--- software/scripts/install.sh
+++ software/scripts/install.sh
@@ -24,5 +24,6 @@ arduino-cli lib install OneButton --log --config-file $YAML_NAME
 arduino-cli lib install ezLED --log --config-file $YAML_NAME
 arduino-cli lib install ArduinoJson --log --config-file $YAML_NAME
 arduino-cli lib install "Adafruit MCP23008 library" --log --config-file $YAML_NAME
+arduino-cli lib install --zip-path ../libraries/MCP_POT-0.2.2.zip --log --config-file $YAML_NAME
 
 echo "Done"
