daa7b2809fb23dbf927d614f20b852c8e9584df3
Stephen Cooper <ste.cooper@live.co.uk>
2024-11-18 21:46:07 +0000

    force change

--- software/software.ino
+++ software/software.ino
@@ -242,14 +242,5 @@ void loop() {
     
     PSPState_loop();
 
-    // must be run last, after controller processing
-
-    // The main loop must have some kind of "yield to lower priority task" event.
-    // Otherwise, the watchdog will get triggered.
-    // If your main loop doesn't have one, just add a simple `vTaskDelay(1)`.
-    // Detailed info here:
-    // https://stackoverflow.com/questions/66278271/task-watchdog-got-triggered-the-tasks-did-not-reset-the-watchdog-in-time
-
-    //     vTaskDelay(1);
     delay(1);
 }
