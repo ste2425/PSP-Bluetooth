3ccf17e703359cd2473737be1c318a4e5101522c
Stephen Cooper <ste.cooper@live.co.uk>
2025-01-17 21:08:08 +0000

    Added ability for UI to show Pre-release builds

--- installer/src/app/programmer-page/programmer-page.component.ts
+++ installer/src/app/programmer-page/programmer-page.component.ts
@@ -4,6 +4,10 @@ import { CommonModule } from '@angular/common';
 import { ReleaseComponent } from '../release/release.component';
 import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
 import { SerialConnectionFactoryService } from '../services/serial-connection-factory.service';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {FormsModule} from '@angular/forms';
+import { BehaviorSubject, combineLatest, map, shareReplay, withLatestFrom } from 'rxjs';
+import { PanelComponent } from '../panel/panel.component';
 
 @Component({
   selector: 'app-programmer-page',
@@ -11,7 +15,10 @@ import { SerialConnectionFactoryService } from '../services/serial-connection-fa
   imports: [
     MatProgressSpinnerModule,
     ReleaseComponent,
-    CommonModule
+    CommonModule,
+    MatCheckboxModule,
+    FormsModule,
+    PanelComponent
   ],
   templateUrl: './programmer-page.component.html',
   styleUrl: './programmer-page.component.scss'
@@ -21,6 +28,27 @@ export class ProgrammerPageComponent {
     public githubService: GithubService,
     public serialService: SerialConnectionFactoryService
   ) {}
+
+  $incPreRelease = new BehaviorSubject(false);
+  get includePrerelease(): boolean {
+    return this.$incPreRelease.value;
+  }
+  set includePrerelease(v: boolean) {
+    this.$incPreRelease.next(v);
+  }  
+
+  $releases = combineLatest([
+    this.githubService.getReleases().pipe(shareReplay(1)),
+    this.$incPreRelease
+  ]).pipe(
+    map(([releases, incPrerelease]) => {
+      return releases.filter(x => {
+        if (!incPrerelease)
+          return !x.prerelease;
+        else
+          return true;
+      });
+    })
+  );
   
-  $releases = this.githubService.getReleases();
 }
